352e677bf87ba41c8e0c693bec31e99194e0949d
saurabhsatasia <saurabhsatasia@icloud.com>
2021-03-11 23:11:53 +0100

    practice

---
+++ SQLQuery2.sql
@@ -0,0 +1,51 @@
+-------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------- Data Types - ---------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------
+
+
+-- VARCHAR(25) - Is for characters - you cannot apply aggregated functions on this
+-- NVARCHAR (25)  - Is for characters - you cannot apply aggregated functions on this
+-- FLOAT - Is for Number values with decimal; so you can aggregate the data - SUM/AVG/MIN/MAX
+-- Int is for Number swith no decimal places
+-- Date is for dates
+
+-------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------- SELECT Query ---------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------
+
+-- * means all
+USE practice
+SELECT * FROM Opportunities_Data
+SELECT New_Account_NO, Opportunity_ID FROM Opportunities_Data
+
+-- Exm 1 - 1 Condition: =
+SELECT * FROM Opportunities_Data WHERE Product_Category = 'Services' --1269
+
+-- Exm 2 - 1 Condition: <>
+SELECT * FROM Opportunities_Data WHERE Opportunity_Stage != 'Stage - 0' --3390
+
+-- Exm 3 - 1 Condition: IN/NOT IN
+SELECT * FROM Opportunities_Data WHERE Opportunity_Stage IN ('Stage - 0', 'Stage - 1','Stage - 2', 'Stage - 3') 
+SELECT * FROM Opportunities_Data WHERE Opportunity_Stage NOT IN ('Stage - 0', 'Stage - 1','Stage - 2', 'Stage - 3') 
+
+-- Exm 4 - 1 Condition: LIKE/NOT LIKE
+SELECT * FROM Opportunities_Data WHERE New_Opportunity_Name LIKE '%Phase - 1%'
+SELECT * FROM Opportunities_Data WHERE New_Opportunity_Name NOT LIKE '%Phase - 1%'
+
+-- Exm 5 - 1 Condition: AND 
+SELECT * FROM Opportunities_Data WHERE Product_Category='Services' AND Opportunity_Stage='Stage - 5'
+
+-- Exm 6 - 1 Condition: OR
+SELECT * FROM Opportunities_Data WHERE Product_Category='Services' OR Opportunity_Stage='Stage - 5'
+
+-- Exm 7 - 1 Condition: AND/OR
+SELECT * FROM Opportunities_Data WHERE (Product_Category='Services' AND Opportunity_Stage='Stage - 5') OR New_Opportunity_Name LIKE '%Phase - 7%'
+
+-- Exm 8 - 1 Condition: >/<
+SELECT * FROM Opportunities_Data WHERE Est_Opportunity_Value > 50000
+SELECT * FROM Opportunities_Data WHERE Est_Opportunity_Value < '50000'
+
+-- Exm 9 - 1 Condition: BETWEEN
+SELECT * FROM Opportunities_Data WHERE Est_Opportunity_Value BETWEEN '50000' AND '70000'
+
+
